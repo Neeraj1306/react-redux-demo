98408a51677bbcdcf967c4e5ca305807432c18e9
Neeraj <neerajgupta592@gmail.com>
2020-04-29 01:03:47 +0530

    Updates

---
+++ precache-manifest.98b89b3e9f682f40fcc4a608e4ce932a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7969a1b3a7a9af75c303b52caae0dbf",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "3d59017b1df6ee3f934f",
+    "url": "/my-react-app/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "8dcf642911d440a1ce1c",
+    "url": "/my-react-app/static/css/main.7ec4b1df.chunk.css"
+  },
+  {
+    "revision": "3d59017b1df6ee3f934f",
+    "url": "/my-react-app/static/js/2.53909775.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-app/static/js/2.53909775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dcf642911d440a1ce1c",
+    "url": "/my-react-app/static/js/main.4b2a0559.chunk.js"
+  },
+  {
+    "revision": "93c452bcc086906070f0",
+    "url": "/my-react-app/static/js/runtime-main.37c1d33f.js"
+  }
+]);
